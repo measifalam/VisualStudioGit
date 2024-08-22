91667cf154c43284d9e803d1c11f1e7386a200bc
Md Asif Alam <54733354+measifalam@users.noreply.github.com>
2024-08-22 16:14:24 +0530

    implementing Fetch

--- VisualStudioGit/Program.cs
+++ VisualStudioGit/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!, Git from Visual Studio");
 Console.WriteLine("Hello, World!, Modified Git from Github");
-Console.WriteLine("Hello, World!, Git from Visual Studio");
-Console.WriteLine("Hello, World!, Modified Git from Github");
-Console.WriteLine("Hello, World!, Git from Visual Studio");
-Console.WriteLine("Hello, World!, Modified Git from Github");
+Console.WriteLine("Hello, World!, Modified Git from Github for implementing Fetch ");
